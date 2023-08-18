4cb17c9a7a5fffe6bc3bbb0a53511102afa868f8
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2023-08-18 17:21:22 +0200

    First Commit

---
+++ Semana_No_1/main.py
@@ -0,0 +1,174 @@
+#############################################################
+#                 Práctica Primera Semana                   #
+#############################################################
+
+
+# 1) Crear una función que reciba una lista de números, y que devuelva un diccionario dónde cada key sea el elemento
+# de la lista en string y su value sea el mismo elemento en su tipo de dato original,
+# el diccionario debe estar en ordenado de menor a mayor.
+def convert_list_to_dict(received_list: list) -> dict:
+    """ This Function Convert List with Integers Items to Dictionary String Keys and Integers Values.
+
+    :param received_list: This is the List argument of the function with Integers.
+    :type received_list: list, require
+
+    :return: A Dictionary with List item as a Key like as String and the Items of the List as Dictionary Values.
+    :rtype: list
+    """
+    return_dict = {}
+    for key, number in enumerate(received_list):
+        if isinstance(number, int):
+            return_dict[str(number)] = number
+        else:
+            print(f"The '{received_list[key]}' item must be a integer")
+
+    return return_dict
+
+# 2) Crear una función que reciba una lista donde se extraigan solo los diccionarios y los devuelva en otra lista,
+# en caso de que la lista de entrada no tenga dict devolver una lista vacía.
+def extract_dict_from_list(received_list: list) -> list:
+    """ This Function Take from a List ONLY Dictionary in a New List if not Dictionary in List return Empty List.
+
+    :param received_list: This is the List argument of the function with Items.
+    :type received_list: list, require
+
+    :return: A List with Dictionary or Empty List if not Dictionary on the Original List.
+    :rtype: list
+    """
+    return_list = []
+    for key, value in enumerate(received_list):
+        if isinstance(value, dict):
+            return_list.append(value)
+
+    return return_list
+
+# 3) Como obtengo el siguiente elemento de la lista.
+# Elemento a obtener: {'13': 14, '15': 'dieciséis'}
+# lst = ["1", (2), ["3", 4, {"5": 6}],["siete"], "8", {"9": 10, "11": [12, {"13": 14,"15": "dieciséis"}]}]
+def extract_item_from_list(item: any, received_list: list) -> any:
+    """ This Function Extract a Specific Item from a List.
+
+    :param item: This is the Item to extract.
+    :type item: any, require
+
+    :param received_list: This is the List with Items to Extract.
+    :type item: list, require
+
+    :return: The Item to Extract of a List.
+    :rtype: any
+    """
+    for l1 in received_list:
+        if l1 == item:
+            return l1
+        elif isinstance(l1, dict):
+            for l2 in l1.values():
+                if isinstance(l2, list):
+                    for k, lst in enumerate(l2):
+                        if lst == item:
+                            return l2[k]
+                    else:
+                        if l2 == item:
+                            return l2
+        elif isinstance(l1, list):
+            for l2 in l1:
+                if isinstance(l2, dict):
+                    for k, v in l2.items():
+                        if v == item:
+                            return l2.get(k)
+                    else:
+                        if l2 == item:
+                            return l2
+
+
+# 4) Crear una función que reciba una lista de diccionarios
+# y devuelva una nueva lista con todos los valores de cada dict.
+def get_values_from_dict(received_list: list) -> list:
+    """ This Function Take from a List ONLY Dictionary Values and Return a List with Dictionary Values.
+
+    :param received_list: This is the List argument of the function with Items.
+    :type received_list: list, require
+
+    :return: A List with Values of Dictionary or Empty List if not Dictionary not has Values.
+    :rtype: list
+    """
+    return_list = []
+    for value in received_list:
+        if isinstance(value, dict):
+            for k, v in value.items():
+                return_list.append(v)
+
+    return return_list
+
+# 5) Crear una función que reciba un diccionario y devuelva una lista con sus valores siempre y cuando su key sea impar,
+# las keys de este diccionario tienen que ser integers, no añadir values de keys que sean string u otro tipo de dato,
+# si no hay valores de keys impares devolver una lista vacía.
+def get_value_from_odd_key(received_dict: dict) -> list:
+    """ This Function Take from a Odd Key of Dictionary and Return the Values on List.
+
+    :param received_dict: This is the Dictionary argument of the function with Items.
+    :type received_dict: dict, require
+
+    :return: A List with Dictionary Values of Odd Key or Empty List if not have Odd Key Dictionary.
+    :rtype: list
+    """
+    return_list = []
+    for k, v in received_dict.items():
+        if isinstance(k, int):
+            for _ in v:
+                if k % 2 == 1:
+                    return_list.append(v)
+
+    return return_list
+
+# 6) Crear una función que reciba una secuencia (list, string, o tuple) como primer parámetro
+# y reciba "n" como segundo parámetro, debe devolver los últimos "n" elementos de la secuencia,
+# la secuencia debe ser "casteada" a lista.
+def return_last_items(received_seq: any, n: int) -> list:
+    """ This Function Take from a Sequence (List, String or Tuple) the "n" Last Values on a List.
+
+    :param received_seq: This is the Sequence (List, String or Tuple) argument of the function with Items.
+    :type received_seq: any, require
+
+    :return: A List with Values of the Sequence or Empty List if not Dictionary not has Values.
+    :rtype: list
+    """
+    if isinstance(received_seq, list) or isinstance(received_seq, str) or isinstance(received_seq, tuple):
+        return list(received_seq[-n::])
+    else:
+        return []
+
+
+def main() -> None:
+    """ This is the main call function of script
+    :param: N/A
+    :return: None
+    """
+
+    # Práctica No. 1
+    numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
+    print(convert_list_to_dict(numbers_list))
+
+    # Práctica No. 2
+    numbers_list = [1, 2, 3, 4, 5, 6, 7, {"a": 0}]
+    print(extract_dict_from_list(numbers_list))
+
+    # Práctica No. 3
+    lst = ["1", (2,), ["3", 4, {"5": 6}], ["siete"], "8", {"9": 10, "11": [12, {"13": 14, "15": "dieciséis"}]}]
+    item = {"13": 14, "15": "dieciséis"}
+    print(extract_item_from_list(item, lst))
+
+    # Práctica No. 4
+    numbers_list = [1, 2, 3, 4, 5, 6, 7, {"a": 0}]
+    print(get_values_from_dict(numbers_list))
+
+    # Práctica No. 5
+    numbers_dict = {"a": 0, 0: "a", "b": 1, 1: "b", "c": 2, 2: "c", "d": 3, 3: "d"}
+    print(get_value_from_odd_key(numbers_dict))
+
+    # Práctica No. 6
+    seq = (1, 2, 3, 4, 5, 6, 7, 8, ["A", "B", "C"], "Python")
+    print(return_last_items(seq, 4))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Semana_No_1/test.py
@@ -0,0 +1,86 @@
+import unittest
+
+from main import convert_list_to_dict, \
+    extract_dict_from_list, \
+    extract_item_from_list, \
+    get_values_from_dict, \
+    get_value_from_odd_key, \
+    return_last_items
+
+
+class TestConvertListToDict(unittest.TestCase):
+    def test_convert_list_to_dict_ok(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
+        _result = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8}
+        self.assertEqual(convert_list_to_dict(_numbers_list), _result)
+
+    def test_convert_list_to_dict_ko(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
+        _result = {'1': "A", '2': "B", '3': "C", '4': "D", '5': "E", '6': "F", '7': "G", '8': "H"}
+        self.assertNotEqual(convert_list_to_dict(_numbers_list), _result)
+
+
+class TestExtractDictFromList(unittest.TestCase):
+    def test_extract_dict_from_list_ok(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, {"a": 0}]
+        _result = [{'a': 0}]
+        self.assertEqual(extract_dict_from_list(_numbers_list), _result)
+
+    def test_extract_dict_from_list_ko(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, 8]
+        _result = [8]
+        self.assertNotEqual(extract_dict_from_list(_numbers_list), _result)
+
+
+class TestExtractItemFromList(unittest.TestCase):
+    def test_extract_item_from_list_ok(self):
+        _lst = ["1", (2,), ["3", 4, {"5": 6}], ["siete"], "8", {"9": 10, "11": [12, {"13": 14, "15": "dieciséis"}]}]
+        _item = {"13": 14, "15": "dieciséis"}
+        _result = {"13": 14, "15": "dieciséis"}
+        self.assertEqual(extract_item_from_list(_item, _lst), _result)
+
+    def test_extract_item_from_list_ko(self):
+        _lst = ["1", (2,), ["3", 4, {"5": 6}], ["siete"], "8", {"9": 10, "11": [12, {"13": 14, "15": "dieciséis"}]}]
+        _item = {"13": 14, "15": "dieciséis"}
+        _result = {"13": 14, "15": "ocho"}
+        self.assertNotEqual(extract_item_from_list(_item, _lst), _result)
+
+
+class TestConvertListToDict(unittest.TestCase):
+    def test_convert_list_to_dict_ok(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, {"a": 0}]
+        _result = [0]
+        self.assertEqual(get_values_from_dict(_numbers_list), _result)
+
+    def test_convert_list_to_dict_ko(self):
+        _numbers_list = [1, 2, 3, 4, 5, 6, 7, {"a": 0}]
+        _result = [1]
+        self.assertNotEqual(get_values_from_dict(_numbers_list), _result)
+
+
+class TestGetValueFromOddKey(unittest.TestCase):
+    def test_get_value_from_odd_key_ok(self):
+        _numbers_dict = {"a": 0, 0: "a", "b": 1, 1: "b", "c": 2, 2: "c", "d": 3, 3: "d"}
+        _result = ['b', 'd']
+        self.assertEqual(get_value_from_odd_key(_numbers_dict), _result)
+
+    def test_get_value_from_odd_key_ko(self):
+        _numbers_dict = {"a": 0, 0: "a", "b": 1, 1: "b", "c": 2, 2: "c", "d": 3, 3: "d"}
+        _result = ['a', 'c']
+        self.assertNotEqual(get_value_from_odd_key(_numbers_dict), _result)
+
+
+class TestReturnLastItems(unittest.TestCase):
+    def test_return_last_items_ok(self):
+        _seq = (1, 2, 3, 4, 5, 6, 7, 8, ["A", "B", "C"], "Python")
+        _result = [7, 8, ['A', 'B', 'C'], 'Python']
+        self.assertEqual(return_last_items(_seq, 4), _result)
+
+    def test_return_last_items_ko(self):
+        _seq = (1, 2, 3, 4, 5, 6, 7, 8, ["A", "B", "C"], "Python")
+        _result = [1, 2, 3, 4, 5, 6]
+        self.assertNotEqual(return_last_items(_seq, 4), _result)
+
+
+if __name__ == "__main__":
+    unittest.main()
