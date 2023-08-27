026df258cfbc7c6d144cebf6b90c4f3da1180cc0
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2023-08-27 02:17:36 +0200

    Add Excercise No. 3

---
+++ Semana_No_3/src/main.py
@@ -0,0 +1,74 @@
+# 1) Pass the correct arguments to function foo to print the expected outputs:
+# def foo(*args, **kwargs):
+# 	print(f"arguments: {args}, keywords: {kwargs}")
+#
+# foo(?) # arguments: (1,2,3), keywords: {}
+# foo(?) # arguments: (1,2,3), keywords: {'one':1}
+# foo(?) # arguments: (), keywords: {'one': 1, 'two': 2, 'three': 3}
+# foo(?) # arguments: (), keywords: {}
+def correct_args(*args, **kwargs):
+    return f"arguments: {args}, keywords: {kwargs}"
+
+
+# 2) Unpack arguments and keywords to function foo, keep in mind that manually passing positional arguments
+# can be tricky, the order is something important to take into consideration, also is not a good practice
+# to have a signature that long with many parameters define in a function, sometimes this is not in our control,
+# for that reason we need to know how to unpack arguments and keywords when is needed for long signatures define
+# in a function.
+#
+# def foo(id, name, last_name, age, email, username, department):
+# 	pass
+#
+# define arguments and keywords on separate variables
+# arguments: tuple = ? # variable define with the arguments to pass to the function
+# keywords: dict = ? # variable define with the keywords to pass to the function
+#
+# unpack then while doing the call on function foo, remember one call for the args and one call for the kwargs.
+# foo(?) # args call
+# foo(?) # kwargs call
+def unpack_args(*args, **kwargs):
+    if args:
+        return f"arguments: {args}"
+    else:
+        return f"keywords: {kwargs}"
+
+
+# 3) Fix all the calls on function foo, keep in mind the order between positional arguments and
+# also default argument values.
+#
+# fix
+# def foo(id=None, name=None, age):
+# 	pass
+#
+# fix
+# def foo(elements=[], start=1, end):
+# 	pass
+#
+# fix
+# def foo(item={"one": 1}, key="1", default_value=None):
+# 	pass
+#
+# fix
+# def foo(country_initials=("AR","ES","VE","PE","BO"), index, return_complete=False):
+# 	pass
+def foo_0(age, id=None, name=None):
+    pass
+
+
+def foo_1(end, start=1, elements=None):
+    pass
+
+
+def foo_2(key="1", item=None, default_value=None):
+    pass
+
+
+def foo_3(index, return_complete=False, country_initials=("AR", "ES", "VE", "PE", "BO")):
+    pass
+
+
+# 4) Write a function that takes two positional arguments, two default arguments, and lastly takes args and kwargs.
+# Add three examples by calling your function, you can return or print any output that you want, be creative!
+def greet(name, lastname, age=52, city="Alcorcón", *args, **kwargs):
+    return f"Hi, my name is {name} {lastname}. I'm have {age} old and I live in {city}. " \
+           f"My args are {(tuple(v for v in args))} and my kwargs are {kwargs}"
---
+++ Semana_No_3/test/test.py
@@ -0,0 +1,116 @@
+import unittest
+
+from src.main import correct_args, foo_0, foo_1, foo_2, foo_3, unpack_args, greet
+
+
+class TestArgsAndKwargs(unittest.TestCase):
+    def test_yes_args_not_kwargs(self):
+        _args = (1, 2, 3)
+        _kwargs = {}
+        _result = 'arguments: (1, 2, 3), keywords: {}'
+        self.assertEqual(correct_args(*_args, **_kwargs), _result)
+
+    def test_yes_args_and_yes_kwargs(self):
+        _args = (1, 2, 3)
+        _kwargs = {"one": 1}
+        _result = "arguments: (1, 2, 3), keywords: {'one': 1}"
+        self.assertEqual(correct_args(*_args, **_kwargs), _result)
+
+    def test_not_args_and_yes_kwargs(self):
+        _args = ()
+        _kwargs = {"one": 1, "two": 2, "three": 3}
+        _result = "arguments: (), keywords: {'one': 1, 'two': 2, 'three': 3}"
+        self.assertEqual(correct_args(*_args, **_kwargs), _result)
+
+    def test_not_both(self):
+        _args = ()
+        _kwargs = {}
+        _result = "arguments: (), keywords: {}"
+        self.assertEqual(correct_args(*_args, **_kwargs), _result)
+
+
+class TestFoo(unittest.TestCase):
+    def test_foo_0(self):
+        _age = 0
+        _result = None
+        self.assertEqual(foo_0(_age), _result)
+
+    def test_foo_1(self):
+        _end = 0
+        _start = 1
+        _result = None
+        self.assertEqual(foo_1(_end, _start), _result)
+
+    def test_foo_2(self):
+        _key = "0"
+        _result = None
+        self.assertEqual(foo_2(_key), _result)
+
+    def test_foo_3(self):
+        _index = 0
+        _result = None
+        self.assertEqual(foo_3(_index), _result)
+
+
+class TestUnpack(unittest.TestCase):
+    def test_unpack_args(self):
+        _args = (1, 2, 3)
+        _result = 'arguments: (1, 2, 3)'
+        self.assertEqual(unpack_args(*_args), _result)
+
+    def test_unpack_kwargs(self):
+        _kwargs = {"one": 1}
+        _result = "keywords: {'one': 1}"
+        self.assertEqual(unpack_args(**_kwargs), _result)
+
+
+class TestGreet(unittest.TestCase):
+    def test_greet_name_and_lastname(self):
+        _name = "Chamo"
+        _lastname = "Linares"
+        _result = "Hi, my name is Chamo Linares. I'm have 52 old and I live in Alcorcón. " \
+                  "My args are () and my kwargs are {}"
+        self.assertEqual(greet(_name, _lastname), _result)
+
+    def test_greet_age(self):
+        _name = "Chamo"
+        _lastname = "Linares"
+        _age = 21
+        _city = "Caracas"
+        _result = "Hi, my name is Chamo Linares. I'm have 21 old and I live in Caracas. " \
+                  "My args are () and my kwargs are {}"
+        self.assertEqual(greet(_name, _lastname, age=_age, city=_city), _result)
+
+    def test_greet_args(self):
+        _name = "Chamo"
+        _lastname = "Linares"
+        _age = 21
+        _city = "Caracas"
+        _args = (1, 2, 3, 4)
+        _result = "Hi, my name is Chamo Linares. I'm have 21 old and I live in Caracas. " \
+                  "My args are (1, 2, 3, 4) and my kwargs are {}"
+        self.assertEqual(greet(_name, _lastname, _age, _city, *_args), _result)
+
+    def test_greet_kwargs(self):
+        _name = "Chamo"
+        _lastname = "Linares"
+        _age = 21
+        _city = "Caracas"
+        _one = 1
+        _two = 2
+        _three = 3
+        _result = "Hi, my name is Chamo Linares. I'm have 21 old and I live in Caracas. " \
+                  "My args are () and my kwargs are {'one': 1, 'two': 2, 'three': 3}"
+        self.assertEqual(greet(_name, _lastname, _age, _city, one=_one, two=_two, three=_three), _result)
+
+    def test_greet_all(self):
+        _name = "Chamo"
+        _lastname = "Linares"
+        _age = 21
+        _city = "Caracas"
+        _args = (1, 2, 3, 4)
+        _one = 1
+        _two = 2
+        _result = "Hi, my name is Chamo Linares. I'm have 21 old and I live in Caracas. " \
+                  "My args are (1, 2, 3, 4) and my kwargs are {'one': 1, 'two': 2}"
+        self.assertEqual(greet(_name, _lastname, _age, _city, *_args, one=_one, two=_two), _result)
